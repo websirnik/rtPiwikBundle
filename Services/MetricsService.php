189e185bb626ede8ef8be822f45120ab381ed4a5
Nikita Korotaev <websirnik@gmail.com>
2018-04-17 23:04:46 +0100

    refactor total metrics

--- Services/MetricsService.php
+++ Services/MetricsService.php
@@ -2,88 +2,86 @@
 
 namespace rtPiwikBundle\Services;
 
+class MetricsService {
+	private $metric;
+	private $analytics;
 
-class MetricsService
-{
-    private $metric;
-    private $analytics;
+	function __construct($analytics) {
+		$this->analytics = $analytics;
+		$this->metric = new MetricModel();
+	}
 
-    function __construct($analytics)
-    {
-        $this->analytics = $analytics;
-        $this->metric = new MetricModel();
-    }
+	public function getSlugs($dateFrom, $dateTo, $userIds) {
+		$slugs = [];
+		$date = $dateFrom . ',' . $dateTo;
 
-    public function getSlugs($dateFrom, $dateTo, $userIds)
-    {
-        $slugs = [];
-        $date = $dateFrom.','.$dateTo;
+		$analyticsEntryPages = $this->analytics->getEntryPages($date, $userIds);
+		foreach ($analyticsEntryPages as $action) {
+			if (isset($action["subtable"])) {
+				foreach ($action["subtable"] as $subtable) {
+					$s = $subtable["label"];
+					if (strpos($s, '?') !== false) {
+						$slugSplitted = explode("?", $s);
+						$s = $slugSplitted[0];
+					}
 
-        $analyticsEntryPages = $this->analytics->getEntryPages($date, $userIds);
-        foreach ($analyticsEntryPages as $action) {
-            if (isset($action["subtable"])) {
-                foreach ($action["subtable"] as $subtable) {
-                    $s = $subtable["label"];
-                    if (strpos($s, '?') !== false) {
-                        $slugSplitted = explode("?", $s);
-                        $s = $slugSplitted[0];
-                    }
+					if ($s[0] == "/") {
+						$slugSplitted = explode("/", $s);
+						$s = $slugSplitted[1];
+					}
 
-                    if ($s[0] == "/") {
-                        $slugSplitted = explode("/", $s);
-                        $s = $slugSplitted[1];
-                    }
+					if (!in_array($s, $slugs)) {
+						array_push($slugs, $s);
+					}
+				}
+			}
+		}
 
-                    if (!in_array($s, $slugs)) {
-                        array_push($slugs, $s);
-                    }
-                }
-            }
-        }
+		return $slugs;
+	}
 
-        return $slugs;
-    }
+	public function getMetrics($slug, $dateFrom, $dateTo, $userIds) {
 
-    public function getMetrics($slug, $dateFrom, $dateTo, $userIds)
-    {
-        $date = $dateFrom.','.$dateTo;
-        $visits = 0;
-        $analyticsMetrics = $this->analytics->getMetrics($slug, $date, $userIds);
-        foreach ($analyticsMetrics as $key => $m) {
-            if (count($m) > 0 && isset($m["nb_visits"])) {
-                $visits += $m["nb_visits"];
-            }
-        }
-        $this->metric->setVisits($visits);
+		$metric = new MetricModel();
 
+		$visits = 0;
+		$timeSpent = 0;
+		$pageViews = 0;
+		$avgTimeSpent = 0;
+		$interactions = 0;
+		$date = $dateFrom . ',' . $dateTo;
 
-        $analyticsActions = $this->analytics->getActions($slug, $date, $userIds);
+		$analyticsMetrics = $this->analytics->getMetrics($slug, $date, $userIds);
+		foreach ($analyticsMetrics as $key => $m) {
+			if (count($m) > 0 && isset($m["nb_visits"])) {
+				$visits += $m["nb_visits"];
+			}
+		}
 
+		$analyticsActions = $this->analytics->getActions($slug, $date, $userIds);
 
-        $timeSpent = 0;
-        foreach ($analyticsActions as $key => $action) {
-            if (count($action) > 0 && isset($action["sum_time_spent"]) && $action["sum_time_spent"] > 0) {
+		foreach ($analyticsActions as $key => $action) {
+			if (count($action) > 0 && isset($action["sum_time_spent"]) && $action["sum_time_spent"] > 0) {
+				$pageViews += $action["nb_hits"];
+				$timeSpent += $action["sum_time_spent"];
+			}
+		}
 
-                $pageViews = $this->metric->getPageViews() + $action["nb_hits"];
-                $this->metric->setPageViews($pageViews);
+		if ($visits > 0) {
+			$avgTimeSpent = round($timeSpent / $this->metric->getVisits());
+		}
 
-                $timeSpent += $action["sum_time_spent"];
-            }
-        }
+		$analyticsInteractions = $this->analytics->getInteractions($slug, $date);
 
-        if ($this->metric->getVisits() > 0) {
-            $avgTimeSpent = round($timeSpent / $this->metric->getVisits());
-            $this->metric->setAvgTimeSpent($avgTimeSpent);
-        }
+		if (isset($analyticsInteractions[0]["nb_events"])) {
+			$interactions = $analyticsInteractions[0]["nb_events"];
+		}
 
-        $analyticsInteractions = $this->analytics->getInteractions($slug, $date);
+		$metric->setPageViews($pageViews);
+		$metric->setVisits($visits);
+		$metric->setAvgTimeSpent($avgTimeSpent);
+		$metric->setInteractions($interactions);
 
-
-        if (isset($analyticsInteractions[0]["nb_events"])) {
-            $interactions = $analyticsInteractions[0]["nb_events"];
-            $this->metric->setInteractions($interactions);
-        }
-
-        return $this->metric;
-    }
+		return $this->metric;
+	}
 }
