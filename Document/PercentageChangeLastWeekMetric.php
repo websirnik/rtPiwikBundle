2e6c3949ede031c9230a1308d942a322c6ca5b43
Nikita Korotaev <websirnik@gmail.com>
2018-04-17 18:11:10 +0100

    add jms service

--- Document/PercentageChangeLastWeekMetric.php
+++ Document/PercentageChangeLastWeekMetric.php
@@ -9,118 +9,118 @@
 namespace rtPiwikBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use JMS\Serializer\Annotation as JMS;
 
 /**
  * @MongoDB\EmbeddedDocument
  */
-class PercentageChangeLastWeekMetric
-{
+class PercentageChangeLastWeekMetric {
 
-    /**
-     * @MongoDB\Field(type="float")
-     */
-    protected $visits;
+	/**
+	 * @JMS\Groups({"metrics"})
+	 * @JMS\Type("float")
+	 * @MongoDB\Field(type="float")
+	 */
+	protected $visits;
 
-    /**
-     * @MongoDB\Field(type="float")
-     */
-    protected $pageViews;
+	/**
+	 * @JMS\Groups({"metrics"})
+	 * @JMS\Type("float")
+	 * @MongoDB\Field(type="float")
+	 */
+	protected $pageViews;
 
-    /**
-     * @MongoDB\Field(type="float")
-     */
-    protected $interactions;
+	/**
+	 * @JMS\Groups({"metrics"})
+	 * @JMS\Type("float")
+	 * @MongoDB\Field(type="float")
+	 */
+	protected $interactions;
 
-    /**
-     * @MongoDB\Field(type="float")
-     */
-    protected $avgTimeSpent;
+	/**
+	 * @JMS\Groups({"metrics"})
+	 * @JMS\Type("float")
+	 * @MongoDB\Field(type="float")
+	 */
+	protected $avgTimeSpent;
 
-    /**
-     * Set visits
-     *
-     * @param float $visits
-     * @return $this
-     */
-    public function setVisits($visits)
-    {
-        $this->visits = $visits;
-        return $this;
-    }
+	/**
+	 * Set visits
+	 *
+	 * @param float $visits
+	 * @return $this
+	 */
+	public function setVisits($visits) {
+		$this->visits = $visits;
+		return $this;
+	}
 
-    /**
-     * Get visits
-     *
-     * @return float $visits
-     */
-    public function getVisits()
-    {
-        return $this->visits;
-    }
+	/**
+	 * Get visits
+	 *
+	 * @return float $visits
+	 */
+	public function getVisits() {
+		return $this->visits;
+	}
 
-    /**
-     * Set pageViews
-     *
-     * @param float $pageViews
-     * @return $this
-     */
-    public function setPageViews($pageViews)
-    {
-        $this->pageViews = $pageViews;
-        return $this;
-    }
+	/**
+	 * Set pageViews
+	 *
+	 * @param float $pageViews
+	 * @return $this
+	 */
+	public function setPageViews($pageViews) {
+		$this->pageViews = $pageViews;
+		return $this;
+	}
 
-    /**
-     * Get pageViews
-     *
-     * @return float $pageViews
-     */
-    public function getPageViews()
-    {
-        return $this->pageViews;
-    }
+	/**
+	 * Get pageViews
+	 *
+	 * @return float $pageViews
+	 */
+	public function getPageViews() {
+		return $this->pageViews;
+	}
 
-    /**
-     * Set interactions
-     *
-     * @param float $interactions
-     * @return $this
-     */
-    public function setInteractions($interactions)
-    {
-        $this->interactions = $interactions;
-        return $this;
-    }
+	/**
+	 * Set interactions
+	 *
+	 * @param float $interactions
+	 * @return $this
+	 */
+	public function setInteractions($interactions) {
+		$this->interactions = $interactions;
+		return $this;
+	}
 
-    /**
-     * Get interactions
-     *
-     * @return float $interactions
-     */
-    public function getInteractions()
-    {
-        return $this->interactions;
-    }
+	/**
+	 * Get interactions
+	 *
+	 * @return float $interactions
+	 */
+	public function getInteractions() {
+		return $this->interactions;
+	}
 
-    /**
-     * Set avgTimeSpent
-     *
-     * @param float $avgTimeSpent
-     * @return $this
-     */
-    public function setAvgTimeSpent($avgTimeSpent)
-    {
-        $this->avgTimeSpent = $avgTimeSpent;
-        return $this;
-    }
+	/**
+	 * Set avgTimeSpent
+	 *
+	 * @param float $avgTimeSpent
+	 * @return $this
+	 */
+	public function setAvgTimeSpent($avgTimeSpent) {
+		$this->avgTimeSpent = $avgTimeSpent;
+		return $this;
+	}
 
-    /**
-     * Get avgTimeSpent
-     *
-     * @return float $avgTimeSpent
-     */
-    public function getAvgTimeSpent()
-    {
-        return $this->avgTimeSpent;
-    }
+	/**
+	 * Get avgTimeSpent
+	 *
+	 * @return float $avgTimeSpent
+	 */
+	public function getAvgTimeSpent() {
+		return $this->avgTimeSpent;
+	}
 }
--- Document/TotalMetric.php
+++ Document/TotalMetric.php
@@ -9,128 +9,127 @@
 namespace rtPiwikBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use JMS\Serializer\Annotation as JMS;
 
 /**
  * @MongoDB\EmbeddedDocument
  */
-class TotalMetric
-{
+class TotalMetric {
 
-    /**
-     * @MongoDB\Field(type="int")
-     */
-    protected $visits;
+	/**
+	 * @JMS\Groups({"metrics"})
+	 * @JMS\Type("integer")
+	 * @MongoDB\Field(type="int")
+	 */
+	protected $visits;
 
-    /**
-     * @MongoDB\Field(type="int")
-     */
-    protected $pageViews;
+	/**
+	 * @JMS\Groups({"metrics"})
+	 * @JMS\Type("integer")
+	 * @MongoDB\Field(type="int")
+	 */
+	protected $pageViews;
 
-    /**
-     * @MongoDB\Field(type="int")
-     */
-    protected $interactions;
+	/**
+	 * @JMS\Groups({"metrics"})
+	 * @JMS\Type("integer")
+	 * @MongoDB\Field(type="int")
+	 */
+	protected $interactions;
 
-    /**
-     * @MongoDB\Field(type="int")
-     */
-    protected $avgTimeSpent;
+	/**
+	 * @JMS\Groups({"metrics"})
+	 * @JMS\Type("integer") *
+	 * @MongoDB\Field(type="int")
+	 */
+	protected $avgTimeSpent;
 
-    /**
-     * Set visits
-     *
-     * @param int $visits
-     * @return $this
-     */
-    public function setVisits($visits)
-    {
-        $this->visits = $visits;
-        return $this;
-    }
+	/**
+	 * Set visits
+	 *
+	 * @param int $visits
+	 * @return $this
+	 */
+	public function setVisits($visits) {
+		$this->visits = $visits;
+		return $this;
+	}
 
-    /**
-     * Get visits
-     *
-     * @return int $visits
-     */
-    public function getVisits()
-    {
-        return $this->visits;
-    }
+	/**
+	 * Get visits
+	 *
+	 * @return int $visits
+	 */
+	public function getVisits() {
+		return $this->visits;
+	}
 
-    /**
-     * Set pageViews
-     *
-     * @param int $pageViews
-     * @return $this
-     */
-    public function setPageViews($pageViews)
-    {
-        $this->pageViews = $pageViews;
-        return $this;
-    }
+	/**
+	 * Set pageViews
+	 *
+	 * @param int $pageViews
+	 * @return $this
+	 */
+	public function setPageViews($pageViews) {
+		$this->pageViews = $pageViews;
+		return $this;
+	}
 
-    /**
-     * Get pageViews
-     *
-     * @return int $pageViews
-     */
-    public function getPageViews()
-    {
-        return $this->pageViews;
-    }
+	/**
+	 * Get pageViews
+	 *
+	 * @return int $pageViews
+	 */
+	public function getPageViews() {
+		return $this->pageViews;
+	}
 
-    /**
-     * Set interactions
-     *
-     * @param int $interactions
-     * @return $this
-     */
-    public function setInteractions($interactions)
-    {
-        $this->interactions = $interactions;
-        return $this;
-    }
+	/**
+	 * Set interactions
+	 *
+	 * @param int $interactions
+	 * @return $this
+	 */
+	public function setInteractions($interactions) {
+		$this->interactions = $interactions;
+		return $this;
+	}
 
-    /**
-     * Get interactions
-     *
-     * @return int $interactions
-     */
-    public function getInteractions()
-    {
-        return $this->interactions;
-    }
+	/**
+	 * Get interactions
+	 *
+	 * @return int $interactions
+	 */
+	public function getInteractions() {
+		return $this->interactions;
+	}
 
-    /**
-     * Set avgTimeSpent
-     *
-     * @param int $avgTimeSpent
-     * @return $this
-     */
-    public function setAvgTimeSpent($avgTimeSpent)
-    {
-        $this->avgTimeSpent = $avgTimeSpent;
-        return $this;
-    }
+	/**
+	 * Set avgTimeSpent
+	 *
+	 * @param int $avgTimeSpent
+	 * @return $this
+	 */
+	public function setAvgTimeSpent($avgTimeSpent) {
+		$this->avgTimeSpent = $avgTimeSpent;
+		return $this;
+	}
 
-    /**
-     * Get avgTimeSpent
-     *
-     * @return int $avgTimeSpent
-     */
-    public function getAvgTimeSpent()
-    {
-        return $this->avgTimeSpent;
-    }
+	/**
+	 * Get avgTimeSpent
+	 *
+	 * @return int $avgTimeSpent
+	 */
+	public function getAvgTimeSpent() {
+		return $this->avgTimeSpent;
+	}
 
-    /**
-     * Get id
-     *
-     * @return id $id
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Get id
+	 *
+	 * @return id $id
+	 */
+	public function getId() {
+		return $this->id;
+	}
 }
